84960ec083f8328163e4745c5b3adf4a65c35dbb
Octavious2 <octaviousngabirano@gmail.com>
2026-02-23 09:28:50 +0000

    feat: add Space Grotesk typography and install expo-font dependencies

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import{StyleSheet, Text, View} from 'react-native';
+import{StyleSheet, Text, View, ActivityIndicator,} from 'react-native';
+import {
+  useFonts,
+  SpaceGrotesk_400Regular,
+  SpaceGrotesk_700Bold
+} from '@expo-google-fonts/space-grotesk';
 import { Theme } from './src/theme/colors';
 
 
 export default function App() {
+
+  // load the fonts hook
+  let fontsLoaded = useFonts({
+    'SpaceGrotesk-Regular': SpaceGrotesk_400Regular,
+    'SpaceGrotesk-Bold': SpaceGrotesk_700Bold,
+  });
+
+  // show a spinner if the fonts arent ready(loading state)
+if(!fontsLoaded){
   return (
+    <View style={[styles.container, {justifyContent: 'center'}]}>
+      <ActivityIndicator size="large" color={Theme.colors.primary} />
+      /</View>
+  );
+}
+    return (
     <View style={styles.container}>
       <Text style={styles.brandText}>BudgetWise</Text>
-      {/* cards placeholder */}
+
       <View style={styles.statusIndicator}>
         <Text style={styles.subText}>Track spending set budgets and manage your finances all from one app</Text>
       </View>
@@ -23,12 +44,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   brandText: {
     color: Theme.colors.primary, 
     fontSize: 40,
     fontWeight: '900',
     letterSpacing: -1,
+    fontFamily: 'SpaceGrotesk-Bold',
   },
+
   statusIndicator: {
     marginTop: 10,
     paddingHorizontal: 12,
@@ -38,6 +62,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'rgba(249, 115, 22, 0.2)',
   },
+
   subText: {
     color: Theme.colors.textSecondary,
     fontSize: 14,
@@ -45,5 +70,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     lineHeight: 25,
     marginHorizontal: 40,
+    fontFamily: 'SpaceGrotesk-Regular',
   },
 });
